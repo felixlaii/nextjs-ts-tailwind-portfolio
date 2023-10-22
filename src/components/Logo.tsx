f91efd784067eefae34e6e2e67bf97db126beb94
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-22 17:02:20 -0400

    added transition and image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,10 +8,25 @@ const Logo = () => {
     useEffect(() => {
         setIsFadeIn(true);
       }, []);
-      
+
     return (
         <div>
-
+     {isFadeIn ? (
+        <Transition
+          show={isFadeIn}
+          enter="transition transform duration-1000"
+          enterFrom="opacity-0 translate-x-[-50%]"
+          enterTo="opacity-100 translate-x-0"
+        >
+          <Image
+            src="/images/blue-logo-1.png"
+            alt="logo"
+            width={150}
+            height={150}
+            // className={`animate__animated animate__tada opacity-1`}
+          />
+        </Transition>
+      ) : null}
         </div>
     )
 }
