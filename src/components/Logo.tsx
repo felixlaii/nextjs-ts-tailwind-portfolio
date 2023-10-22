a741a8c1ac53f429f5e122e32da20619f0f53444
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-22 17:00:22 -0400

    useeffect for fade animation

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,6 +5,10 @@ import { Transition } from "@headlessui/react";
 const Logo = () => {
     const [isFadeIn, setIsFadeIn] = useState(false);
 
+    useEffect(() => {
+        setIsFadeIn(true);
+      }, []);
+      
     return (
         <div>
 
