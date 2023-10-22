22e7bffc79a82014d75b4a13f6fb9915dbf27ea4
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-22 16:22:43 -0400

    added nav links and imported nav types

--- src/components/layout/ProjectLayout.tsx
+++ src/components/layout/ProjectLayout.tsx
@@ -4,6 +4,7 @@ import Footer from "./project-layout/Footer";
 import { Header } from "./project-layout/Header";
 import { Wrapper } from "./project-layout/Wrapper";
 import { useRouter } from "next/router";
+import { NavigationLink } from "@/types/component-types";
 import clsx from "clsx";
 
 
@@ -14,6 +15,16 @@ import clsx from "clsx";
  */
 export const ProjectLayout: React.FC<PropsWithChildren> = ({ children }, props ) => {
   const location = useRouter();
+  const navigationLinks: Array<NavigationLink> = [
+    { name: "ABOUT", href: "/about" },
+    {
+      name: "PROJECTS",
+      href: "/projects",
+    },
+    { name: "SKILLS", href: "/skills" },
+    // { name: "GALLERY", href: "#", dropdown: GALLERY_DROPDOWN },
+    // { name: "CONTACT US", href: "/contact-us" },
+  ];
   return (
     <Wrapper>
       <Header />
