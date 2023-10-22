02968497d16c5f4c5584d5b845ee8ebeb6e2f056
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-22 18:39:19 -0400

    added page heading

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,43 +1,47 @@
 import React from "react";
 import { SkillsData } from "../../data/skills-data";
 import Image from "next/image";
+import PageHeading from "@/components/ui/PageHeading";
 
 const Skills = () => {
-    // Split SkillsData array into two parts: 8 items for the top row, and 8 items for the bottom row
-    const topRowSkills = SkillsData.slice(0, 8);
-    const bottomRowSkills = SkillsData.slice(8, 16);
+  // Split SkillsData array into two parts: 8 items for the top row, and 8 items for the bottom row
+  const topRowSkills = SkillsData.slice(0, 8);
+  const bottomRowSkills = SkillsData.slice(8, 16);
 
-    return (
-        <div>
-            {/* Top Row */}
-            <div>
-                <ul className="flex justify-between ml-10 mr-10">
-                    {topRowSkills.map((skill, index) => (
-                        <li className="flex-row mt-28" key={skill.image}>
-                            <div className="flex-col items-center">
-                            <Image width={70} height={70} src={skill.image} alt="skills" />
-                            <p>{skill.description}</p>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+  return (
+    <div>
+      <div className="relative pt-10 sm:pt-0 h-[14rem] md:h-[18rem]">
+        <PageHeading title="Skills & Tools" />
+      </div>
+      {/* Top Row */}
+      <div>
+        <ul className="flex justify-between ml-10 mr-10">
+          {topRowSkills.map((skill, index) => (
+            <li className="flex-row mt-28" key={skill.image}>
+              <div className="flex-col items-center">
+                <Image width={70} height={70} src={skill.image} alt="skills" />
+                <p>{skill.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-            {/* Bottom Row */}
-            <div>
-                <ul className="flex justify-between mt-20 ml-10 mr-10">
-                    {bottomRowSkills.map((skill, index) => (
-                        <li className="flex-row" key={skill.image}>
-                          <div className="flex-col">
-                            <Image width={70} height={70} src={skill.image} alt="skills" />
-                            <p>{skill.description}</p>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+      {/* Bottom Row */}
+      <div>
+        <ul className="flex justify-between mt-20 ml-10 mr-10">
+          {bottomRowSkills.map((skill, index) => (
+            <li className="flex-row" key={skill.image}>
+              <div className="flex-col">
+                <Image width={70} height={70} src={skill.image} alt="skills" />
+                <p>{skill.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
