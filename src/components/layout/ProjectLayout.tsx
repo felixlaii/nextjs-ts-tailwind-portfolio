fd1992fbc94250338de852802c4e75434f76da1d
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-22 16:20:42 -0400

    added types and location

--- src/components/layout/ProjectLayout.tsx
+++ src/components/layout/ProjectLayout.tsx
@@ -3,12 +3,17 @@ import { Content } from "./project-layout/Content";
 import Footer from "./project-layout/Footer";
 import { Header } from "./project-layout/Header";
 import { Wrapper } from "./project-layout/Wrapper";
+import { useRouter } from "next/router";
+import clsx from "clsx";
+
+
 
 /**
  * Responsive web UI layout for RheumInfo.
  * Includes a header with responsive navigation menu and a footer.
  */
-export const ProjectLayout: React.FC<PropsWithChildren> = ({ children }) => {
+export const ProjectLayout: React.FC<PropsWithChildren> = ({ children }, props ) => {
+  const location = useRouter();
   return (
     <Wrapper>
       <Header />
