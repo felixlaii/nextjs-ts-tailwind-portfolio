d07a41a865c4ad3210c58110a63a4d80113edb6e
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-22 16:59:58 -0400

    added usestate

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,3 +2,14 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Transition } from "@headlessui/react";
 
+const Logo = () => {
+    const [isFadeIn, setIsFadeIn] = useState(false);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Logo;
