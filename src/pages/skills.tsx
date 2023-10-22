9b8bc7303ebf59bfef8e38290cbacb4934c31ddc
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-22 18:33:27 -0400

    split array and styled

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -3,24 +3,41 @@ import { SkillsData } from "../../data/skills-data";
 import Image from "next/image";
 
 const Skills = () => {
-        // Calculate the midpoint of the SkillsData array
-        const midpoint = Math.ceil(SkillsData.length / 2);
+    // Split SkillsData array into two parts: 8 items for the top row, and 8 items for the bottom row
+    const topRowSkills = SkillsData.slice(0, 8);
+    const bottomRowSkills = SkillsData.slice(8, 16);
 
-         // Split the SkillsData array into two separate arrays
-    const firstRowSkills = SkillsData.slice(0, midpoint);
-    const secondRowSkills = SkillsData.slice(midpoint);
     return (
         <div>
-            <ul className="flex">
-                {SkillsData.map((skill, index) => (
-                    <li className="flex-row mt-20" key={skill.image}>
-                        <Image width={150}
-            height={150} src={skill.image} alt="skills" />
-            <p>{skill.description}</p>
-                    </li>
-                ))}
-            </ul>
+            {/* Top Row */}
+            <div>
+                <ul className="flex justify-between ml-10 mr-10">
+                    {topRowSkills.map((skill, index) => (
+                        <li className="flex-row mt-28" key={skill.image}>
+                            <div className="flex-col items-center">
+                            <Image width={70} height={70} src={skill.image} alt="skills" />
+                            <p>{skill.description}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            {/* Bottom Row */}
+            <div>
+                <ul className="flex justify-between mt-20 ml-10 mr-10">
+                    {bottomRowSkills.map((skill, index) => (
+                        <li className="flex-row" key={skill.image}>
+                          <div className="flex-col">
+                            <Image width={70} height={70} src={skill.image} alt="skills" />
+                            <p>{skill.description}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-    )
-}
+    );
+};
+
 export default Skills;
