2bb4f5e7ac489024574923b1bc365b92e9a0a654
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-22 17:57:38 -0400

    added skills data

---
+++ data/skills-data.ts
@@ -0,0 +1,69 @@
+export const SkillsData: { image: string, description: string, }[] = [
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        description: "React"
+    },
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+        description: "JavaScript"
+    },
+  
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        description: "TypeScript"
+    },
+  
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        description: "HTML5"
+    },
+  
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        description: "CSS3"
+    },
+  
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+        description: "SASS"
+    },
+  
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg",
+        description: "RESTful API's"
+    },
+  
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        description: "GitHub"
+    },
+
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg",
+        description: "Wordpress"
+    },
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        description: "Node.js"
+    },
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+        description: "MySQL"
+    },    
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+        description: "Express"
+    },
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+        description: "Firebase"
+    },
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
+        description: "Next.js"
+    },
+    {
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg",
+        description: "Tailwindcss"
+    }
+]
