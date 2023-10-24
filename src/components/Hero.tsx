750c38c3e1b924fde820e62c2ef07dc059e23c2a
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 18:24:00 -0400

    centered words and image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,12 +2,20 @@ import Image from "next/image";
 const Hero: React.FC = () => {
   return (
     <div className="bg-brand-darkest max-h-full mt-16">
-      <Image
-        width={200}
-        height={200}
-        src="/images/hero-image.png"
-        alt="Hero Image"
-      />
+      <div className="flex flex-row items-center">
+        <div>
+          <Image
+            width={200}
+            height={200}
+            src="/images/hero-image.png"
+            alt="Hero Image"
+          />
+        </div>
+        <div >
+          <p>software developer</p>
+          <p>& dog lover</p>
+        </div>
+      </div>
     </div>
   );
 };
