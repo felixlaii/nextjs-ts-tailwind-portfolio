f473ddf693a9b6afa8145c70278ae0d2bbfd70b7
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 15:14:41 -0400

    added images for projects

--- data/projects-data.ts
+++ data/projects-data.ts
@@ -1,6 +1,6 @@
 export const ProjectsData: { image: string, description: string }[] = [
     {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174362/pawfinder-static_pvnjkw.png", description: "An application that compiles all adoption agency information into one convenient location. Login and save your pet preferences. Adopt and save a pet today! Ability to sign up and save adoption preferences such as breed and age."},
-    {image: "", description: ""},
-    {image: "", description: ""},
-    {image: "", description: ""},
+    {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174707/logo_qh1koz.svg", description: ""},
+    {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174744/mix6ix2022_blk_ivehqs.png", description: ""},
+    {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174855/stpatrick-logo_bd3mth.png", description: ""},
 ]
