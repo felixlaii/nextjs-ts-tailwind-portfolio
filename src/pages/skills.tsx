27b8c2cf47323a4469f1f64e22ef125cb074cd2e
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 18:51:47 -0400

    adjusted flex

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -9,18 +9,26 @@ const Skills = () => {
   const bottomRowSkills = SkillsData.slice(8, 16);
 
   return (
-    <div className="bg-brand-dark">
-      {/* <div className="relative pt-10 sm:pt-0 h-[14rem] md:h-[18rem]">
-        <PageHeading title="Skills & Tools" />
-      </div> */}
+    <div className="bg-brand-dark pb-20">
+      <div>
+        <h2>Skills & Tools</h2>
+        <p>My Toolbox & Things I can do</p>
+        <p>The skills, tools and technologies I use to bring your vision to life</p>
+      </div>
       {/* Top Row */}
       <div>
-        <ul className="flex justify-between ml-10 mr-10">
+        <ul className="flex justify-between ml-16 mr-16">
           {topRowSkills.map((skill, index) => (
-            <li className="flex-row mt-28" key={skill.image}>
-              <div className="flex-col items-center">
+            <li className="flex-row mt-28 text-white" key={skill.image}>
+              <div className="flex-col justify-center">
+                <div className="flex flex-col justify-center">
+                  <div>
                 <Image width={70} height={70} src={skill.image} alt="skills" />
+                </div>
+                <div className="flex justify-center">
                 <p>{skill.description}</p>
+                </div>
+                </div>
               </div>
             </li>
           ))}
@@ -31,10 +39,14 @@ const Skills = () => {
       <div>
         <ul className="flex justify-between mt-20 ml-10 mr-10">
           {bottomRowSkills.map((skill, index) => (
-            <li className="flex-row" key={skill.image}>
+            <li className="flex-row text-white justify-center" key={skill.image}>
               <div className="flex-col">
+                <div>
                 <Image width={70} height={70} src={skill.image} alt="skills" />
+                </div>
+                <div className="flex justify-center">
                 <p>{skill.description}</p>
+                </div>
               </div>
             </li>
           ))}
