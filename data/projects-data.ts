3623a47f8b4a799316a0e435dc35aca6c01cd220
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 15:20:40 -0400

    format document

--- data/projects-data.ts
+++ data/projects-data.ts
@@ -1,7 +1,36 @@
-export const ProjectsData: { image: string, name: string, url: string, description: string }[] = [
-    {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174362/pawfinder-static_pvnjkw.png", name: "pawfinder", url: "https://github.com/felixlaii/pawfinder", description: "An application that compiles all adoption agency information into one convenient location. Login and save your pet preferences. Adopt and save a pet today! Ability to sign up and save adoption preferences such as breed and age."},
-    {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174707/logo_qh1koz.svg", name: "richmond west dental", url: "https://richmondwestdental.com", description: ""},
-    {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174744/mix6ix2022_blk_ivehqs.png", name: 
-    "mix6ix bartending co", url: "https://mix6ixtoronto.com", description: ""},
-    {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174855/stpatrick-logo_bd3mth.png", name:"St Patrick Secondary School", url: "", description: ""},
-]
+export const ProjectsData: {
+  image: string;
+  name: string;
+  url: string;
+  description: string;
+}[] = [
+  {
+    image:
+      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174362/pawfinder-static_pvnjkw.png",
+    name: "pawfinder",
+    url: "https://github.com/felixlaii/pawfinder",
+    description:
+      "An application that compiles all adoption agency information into one convenient location. Login and save your pet preferences. Adopt and save a pet today! Ability to sign up and save adoption preferences such as breed and age.",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174707/logo_qh1koz.svg",
+    name: "richmond west dental",
+    url: "https://richmondwestdental.com",
+    description: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174744/mix6ix2022_blk_ivehqs.png",
+    name: "mix6ix bartending & co",
+    url: "https://mix6ixtoronto.com",
+    description: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174855/stpatrick-logo_bd3mth.png",
+    name: "St Patrick Secondary School",
+    url: "",
+    description: "",
+  },
+];
