020c57a1ba4db44678db7924466b44f68c18e8a6
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 09:46:06 -0400

    added imports

--- src/components/layout/project-layout/Footer.tsx
+++ src/components/layout/project-layout/Footer.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { FooterProps } from "@/types/component-types";
+import { snakeCaseToTitleCase } from "@/lib/functions";
+import { FELIX_DETAILS } from "../../../../data/portfolio-info";
+import {
+  PiTiktokLogoLight,
+  PiFacebookLogo,
+  PiInstagramLogoLight,
+} from "react-icons/pi";
+
 const Footer: React.FC = () => {
   return <div className="w-screen"></div>;
 };
