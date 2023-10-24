68090551d81c5a87cfea614c341a5da1b9a23edc
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 17:19:38 -0400

    re did spacing

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,42 +4,33 @@ import { ProjectsData } from "../../data/projects-data";
 import ProjectCard from "@/components/ui/ProjectCard";
 
 const Projects: React.FC = () => {
-  const topProjects = ProjectsData.slice(0, 2); // First two projects
-  const bottomProjects = ProjectsData.slice(2, 4); // Next two projects
+  const topProjects = ProjectsData.slice(0, 3); // First three projects
+  const bottomProject = ProjectsData[3]; // The fourth project
 
   return (
-    <div>
-      <div className="flex flex-col md:flex-row md:justify-evenly lg:justify-center mt-12 lg:mt-24 md:mb-10">
-        <div className="flex flex-col md:items-center">
-          {topProjects.map((project, index) => (
-            <div
-              key={project.name}
-              className="h-[26rem] border-b-[1px] hover:border-none border-zinc-300 last:border-none mx-10 pt-4"
-            >
-              <ProjectCard
-                name={project.name}
-                url={project.url}
-                description={project.description}
-                image={project.image}
-              />
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-col md:items-center mt-10 md:mt-0">
-          {bottomProjects.map((project, index) => (
-            <div
-              key={project.name}
-              className="h-[26rem] border-b-[1px] hover:border-none border-zinc-300 last:border-none mx-10 pt-4"
-            >
-              <ProjectCard
-                name={project.name}
-                url={project.url}
-                description={project.description}
-                image={project.image}
-              />
-            </div>
-          ))}
-        </div>
+    <div className="flex flex-col items-center mt-12 lg:mt-24">
+      <div className="flex flex-wrap justify-center gap-8">
+        {topProjects.map((project, index) => (
+          <div
+            key={project.name}
+            className="h-[26rem] border-b-[1px] hover:border-none border-zinc-300 last:border-none mx-10 pt-4"
+          >
+            <ProjectCard
+              name={project.name}
+              url={project.url}
+              description={project.description}
+              image={project.image}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="h-[26rem] border-b-[1px] hover:border-none border-zinc-300 last:border-none mx-10 pt-4">
+        <ProjectCard
+          name={bottomProject.name}
+          url={bottomProject.url}
+          description={bottomProject.description}
+          image={bottomProject.image}
+        />
       </div>
     </div>
   );
