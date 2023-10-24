4daef7840214846669974d33f8f374d0d467e234
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 10:21:57 -0400

    added socials

--- src/components/layout/project-layout/Footer.tsx
+++ src/components/layout/project-layout/Footer.tsx
@@ -10,6 +10,10 @@ import {
   PiInstagramLogoLight,
 } from "react-icons/pi";
 
+const iconClassName = "mr-2 w-4 h-4 text-white";
+const divClassName = " flex flex-row justify-center mt-2 mb-1";
+const socialIconClassName = "mt-3 mb-3 mr-4 w-7 h-7 text-brand-gold";
+
 const LogoFooter = () => {
   return (
     <div>
@@ -23,7 +27,28 @@ const LogoFooter = () => {
 const PortfolioSocials = () => {
   return (
     <div>
-      
+       <div className="flex flex-row mb-1">
+        <div>
+          <a href="https://www.instagram.com/mix.6ix/" target="_blank">
+            {" "}
+            <PiInstagramLogoLight className={socialIconClassName} />
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://www.tiktok.com/@mix.6ix?_t=8dTWCTllNv4&_r=1"
+            target="_blank"
+          >
+            <PiTiktokLogoLight className={socialIconClassName} />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.facebook.com/MIXX6IX" target="_blank">
+            {" "}
+            <PiFacebookLogo className={socialIconClassName} />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
