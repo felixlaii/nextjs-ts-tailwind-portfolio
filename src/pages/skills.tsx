1025e4b908efaf9c3e8f8114e3bbf67aee578f87
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 18:52:48 -0400

    deleted unused css

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -20,8 +20,8 @@ const Skills = () => {
         <ul className="flex justify-between ml-16 mr-16">
           {topRowSkills.map((skill, index) => (
             <li className="flex-row mt-28 text-white" key={skill.image}>
-              <div className="flex-col justify-center">
-                <div className="flex flex-col justify-center">
+              <div className="flex-col">
+                <div>
                   <div>
                 <Image width={70} height={70} src={skill.image} alt="skills" />
                 </div>
@@ -39,7 +39,7 @@ const Skills = () => {
       <div>
         <ul className="flex justify-between mt-20 ml-10 mr-10">
           {bottomRowSkills.map((skill, index) => (
-            <li className="flex-row text-white justify-center" key={skill.image}>
+            <li className="flex-row text-white" key={skill.image}>
               <div className="flex-col">
                 <div>
                 <Image width={70} height={70} src={skill.image} alt="skills" />
