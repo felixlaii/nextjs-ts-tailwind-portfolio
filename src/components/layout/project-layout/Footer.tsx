a32480f4c37da0b247933d77e8ebee7128f22901
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 10:12:54 -0400

    created footer logo

--- src/components/layout/project-layout/Footer.tsx
+++ src/components/layout/project-layout/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image"
 import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 import { FooterProps } from "@/types/component-types";
 import { snakeCaseToTitleCase } from "@/lib/functions";
@@ -8,6 +9,24 @@ import {
   PiFacebookLogo,
   PiInstagramLogoLight,
 } from "react-icons/pi";
+
+const LogoFooter = () => {
+  return (
+    <div>
+         <div className="relative">
+        <Image className="h-[6.5rem]" src="/images/blue-logo-1.png" alt="logo" />
+      </div>
+    </div>
+  )
+}
+
+const PortfolioSocials = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 const Footer: React.FC = () => {
   return <div className="w-screen"></div>;
 };
