7bc9a363cba2e985044a80979b0c4cb5c4679850
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 17:26:55 -0400

    Create tailwind-preset.js

---
+++ tailwind-preset.js
@@ -0,0 +1,11 @@
+const { red } = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, "$1")
+    .replace(/\.0$/, "");
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
