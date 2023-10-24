0270d5369ca2eacdfe1442e77fdb7be14948059b
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 15:10:26 -0400

    added pawfinder image and description

--- data/projects-data.ts
+++ data/projects-data.ts
@@ -1,3 +1,6 @@
 export const ProjectsData: { image: string, description: string }[] = [
-    {image: "", description: ""}
+    {image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174362/pawfinder-static_pvnjkw.png", description: "An application that compiles all adoption agency information into one convenient location. Login and save your pet preferences. Adopt and save a pet today! Ability to sign up and save adoption preferences such as breed and age."},
+    {image: "", description: ""},
+    {image: "", description: ""},
+    {image: "", description: ""},
 ]
