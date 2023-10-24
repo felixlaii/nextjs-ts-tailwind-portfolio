c20013c09fa3ebee2f38fc6fa5c9f77510acf2a7
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-24 17:17:07 -0400

    adjusted width and centered cards

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -16,15 +16,13 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
     return (
         <Link
         href={url}
-        className="flex flex-col md:max-w-52 lg:w-80 md:px-3 lg:px-8 pt-4 md:pt-0 h-full rounded-lg md:justify-evenly lg:justify-start text-center lg:mx-auto md:gap-y-4 hover:shadow-xl hover:scale-105 hover:transition hover:duration-300"
+        className="flex flex-col justify-center items-center md:max-w-52 lg:w-80 md:px-3 lg:px-8 pt-4 md:pt-0 h-full rounded-lg md:justify-evenly lg:justify-start text-center lg:mx-auto md:gap-y-4 hover:shadow-xl hover:scale-105 hover:transition hover:duration-300"
         onMouseEnter={() => setIconType("hoverIcon")}
         onMouseLeave={() => setIconType("icon")}
       >
-        {/* <div className="mx-auto pt-4 transition">
-          {iconType === "icon" ? icon : hoverIcon}
-        </div> */}
-        <div className="w-30">
-            <Image className="rounded-lg object-contain" width={200} height={200} src={image} alt="project logo"/>
+
+        <div className="w-200 h-90">
+            <Image className="rounded-lg" width={200} height={90} src={image} alt="project logo"/>
         </div>
   
         <h2 className="text-xl lg:text-2xl font-normal">{name}</h2>
