ea9700d702d4edd4a00ce3d516965f7ad5903d89
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-28 18:00:08 -0400

    switched projects and added style

--- data/projects-data.ts
+++ data/projects-data.ts
@@ -6,13 +6,50 @@ export const ProjectsData: {
   category: "school" | "professional"
   technology: string[];
 }[] = [
+ 
+  {
+    image:
+      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174707/logo_qh1koz.svg",
+    name: "Richmond West Dental",
+    url: "https://richmondwestdental.com",
+    description: "Downtown Toronto dental office. Application deployed on Digital Ocean and built with Nextjs, TypeScript and TailwindCSS.",
+    category: "professional",
+    technology: ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-line.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/digitalocean/digitalocean-original-wordmark.svg",   ]
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698365211/pink_purple_zkqi27.png",
+    name: "MIX6IX Bartending & Co",
+    url: "https://mix6ixtoronto.com",
+    description: "Bartending service for private events. Custom code injections through Square Space.",
+    category: "professional",
+    technology: ["https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698363684/squarespace_logo_icon_181252_o3zs5v.png", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"]
+
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174855/stpatrick-logo_bd3mth.png",
+    name: "St Patrick Secondary School",
+    url: "",
+    description: "Secondary School based in Vancouver.",
+    category: "professional",
+    technology: ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"]
+  },
+  {
+    image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698362504/Screenshot_2023-10-26_at_7.21.40_PM_kewmlp.png",
+    name: "Blush & Letters Co",
+    url: "https://blushnletters.com/",
+    description: "Modern Calligraphy and design. A website done through Square Space with custom code injections to bring client's vision to life.",
+    category: "professional",
+    technology: ["https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698363684/squarespace_logo_icon_181252_o3zs5v.png", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"]
+  },
   {
     image:
       "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174362/pawfinder-static_pvnjkw.png",
     name: "PawFinder",
     url: "https://github.com/felixlaii/pawfinder",
     description:
-      "An application that compiles all adoption agency information into one convenient location. Login and save your pet preferences. Adopt and save a pet today! Ability to sign up and save adoption preferences such as breed and age.",
+      "An application that compiles all adoption agency information into one convenient location. Ability to sign up and save adoption preferences such as breed and age.",
       category: "school",
       technology: ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"   ]
   },
@@ -57,42 +94,6 @@ export const ProjectsData: {
     technology: ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg", "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698363684/squarespace_logo_icon_181252_o3zs5v.png", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"]
   },
 
-  {
-    image:
-      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174707/logo_qh1koz.svg",
-    name: "Richmond West Dental",
-    url: "https://richmondwestdental.com",
-    description: "Downtown Toronto dental office. Application deployed on Digital Ocean and built with Nextjs, TypeScript and TailwindCSS.",
-    category: "professional",
-    technology: ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-line.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/digitalocean/digitalocean-original-wordmark.svg",   ]
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698365211/pink_purple_zkqi27.png",
-    name: "MIX6IX Bartending & Co",
-    url: "https://mix6ixtoronto.com",
-    description: "Bartending service for private events. Custom code injections through Square Space.",
-    category: "professional",
-    technology: ["https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698363684/squarespace_logo_icon_181252_o3zs5v.png", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"]
-
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698174855/stpatrick-logo_bd3mth.png",
-    name: "St Patrick Secondary School",
-    url: "",
-    description: "Secondary School based in Vancouver.",
-    category: "professional",
-    technology: ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"]
-  },
-  {
-    image: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698362504/Screenshot_2023-10-26_at_7.21.40_PM_kewmlp.png",
-    name: "Blush & Letters Co",
-    url: "https://blushnletters.com/",
-    description: "Modern Calligraphy and design. A website done through Square Space with custom code injections to bring client's vision to life.",
-    category: "professional",
-    technology: ["https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698363684/squarespace_logo_icon_181252_o3zs5v.png", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"]
-  }
 
 
 ];
--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -13,7 +13,9 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 
   return (
     <div
-      className="flex flex-col justify-center items-center md:max-w-45 lg:w-64 md:px-3 lg:px-8 pt-4 md:pt-0 h-70 rounded-lg md:justify-evenly lg:justify-start text-center lg:mx-auto md:gap-y-4 hover:shadow-xl hover:scale-105 hover:transition hover:duration-300"
+      className={`flex flex-col justify-center items-center md:max-w-45 lg:w-64 md:px-3 lg:px-8 pt-4 md:pt-0 h-70 rounded-lg md:justify-evenly lg:justify-start text-center lg:mx-auto md:gap-y-4 transition duration-300 shadow-xl transform ${
+        isHovered ? "shadow-xl bg-brand-light scale-105" : ""
+      }`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       style={{ minHeight: "280px" }} // Adjust the minimum height as needed
@@ -23,12 +25,12 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           <div className="flex items-center space-x-2 mt-10">
             {technology.map((iconUrl, index) => (
               <Image
-                height={60}
-                width={60}
+                height={50}
+                width={50}
                 key={index}
                 src={iconUrl}
                 alt={`tech-icon-${index}`}
-                className="w-6 h-6"
+                className="w-4 h-4"
               />
             ))}
           </div>
@@ -45,8 +47,8 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           <div className="w-150 h-90">
             <Image
               className="rounded-lg object-center"
-              width={150}
-              height={90}
+              width={270}
+              height={170}
               src={image}
               alt="project logo"
             />
