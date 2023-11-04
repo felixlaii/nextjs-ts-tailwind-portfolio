d4a37936bada73d6d3f9eea5a9c0fe279117ffa4
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-03 22:25:36 -0400

    created toggle

--- src/components/ui/inputs/DarkModeToggle.tsx
+++ src/components/ui/inputs/DarkModeToggle.tsx
@@ -3,9 +3,13 @@ import { DarkModeProviderProps } from '@/contexts/DarkModeContext';
 
 const DarkModeToggle: React.FC<DarkModeProviderProps> = ({ initialIsDarkMode, toggleDarkMode }) => {
   return (
-    <button onClick={toggleDarkMode}>
-      {initialIsDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
-    </button>
+    <label className="flex items-center cursor-pointer">
+      <div className="relative">
+        <input type="checkbox" className="hidden" checked={initialIsDarkMode} onChange={toggleDarkMode} />
+        <div className="toggle-track w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
+        <div className={`toggle-thumb absolute w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ${initialIsDarkMode ? 'translate-x-full' : 'translate-x-0'}`}></div>
+      </div>
+    </label>
   );
 };
 
