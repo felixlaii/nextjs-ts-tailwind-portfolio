65171394c90198a7bd0ddaa5b05a350fdd9b2261
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-17 17:48:35 -0500

    imported icons and props

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
 import CarouselIndicator from "./CarouselIndicator";
 import CarouselItem from "./CarouselItem";
+import { IoIosArrowBack } from "react-icons/io";
+import { CarouselProps } from "@/types/component-types";
 
+export default function Carousel({ width, height, items }: CarouselProps) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
