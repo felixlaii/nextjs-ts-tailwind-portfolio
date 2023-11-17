6eb64412dba40fa8604b772e5e59b8364caaad11
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-17 17:43:28 -0500

    created maxindicator

--- src/components/CaraouselIndicator.tsx
+++ src/components/CaraouselIndicator.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { CarouselIndicatorProps } from "@/types/component-types";
 
 export default function CarouselIndicator({ activeIndex, length, maxIndicatorVisible = 7, onSetActiveIndex }: CarouselIndicatorProps) {
+    const maxIndicator = length > maxIndicatorVisible ? maxIndicatorVisible : length;
+    
     return (
         <div>
-            
+
         </div>
     )
 }
