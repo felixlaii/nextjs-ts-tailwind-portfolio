c5f63f01e51c5ef1ed71115cc2d024ac2565fa84
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-17 17:42:35 -0500

    Create CaraouselIndicator.tsx

---
+++ src/components/CaraouselIndicator.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CarouselIndicatorProps } from "@/types/component-types";
+
+export default function CarouselIndicator({ activeIndex, length, maxIndicatorVisible = 7, onSetActiveIndex }: CarouselIndicatorProps) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
