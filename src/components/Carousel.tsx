da1084f06b1a67db1c05a4d3cb26d804e0030bae
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-17 17:50:03 -0500

    created function for button

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,7 +5,19 @@ import { IoIosArrowBack } from "react-icons/io";
 import { CarouselProps } from "@/types/component-types";
 
 export default function Carousel({ width, height, items }: CarouselProps) {
-    const [activeIndex, onSetActiveIndex] = useState<number>(0)
+    const [activeIndex, setActiveIndex] = useState<number>(0)
+
+    function handleNextItemBtn() {
+        setActiveIndex((prev) => {
+          return prev + 1 < items.length ? prev + 1 : prev;
+        });
+      }
+    
+      function handlePrevItemBtn() {
+        setActiveIndex((prev) => {
+          return prev - 1 >= 0 ? prev - 1 : prev;
+        });
+      }
     return (
         <div>
 
