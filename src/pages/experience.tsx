9abda9af8968d42997f3ce2a8a3fad3bd5877c72
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-25 14:49:55 -0500

    spacing

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -5,27 +5,36 @@ import Image from "next/image";
 const Experience: React.FC<ProjectCardProps> = () => {
   const router = useRouter();
 
-  // Retrieve project details from query parameters with type assertions
   const id = router.query.id as string;
   const name = router.query.name as string;
   const description = router.query.description as string;
   const image = router.query.image as string;
   const technology = router.query.technology as string[] | undefined; // Adjust type accordingly
   const href = router.query.href as string;
+
   return (
-    <div className="pt-[10rem]">
-      <div>
-        <h2 className="text-[3rem] font-custom">{name}</h2>
-        <span>
-        <Image className="w-[30rem] h-[20rem]"src={image} height={200} width={200} alt="Project Image" />
-      </span>
-        <p className="font-custom">{description}</p>
+    <div className="flex flex-col items-center justify-center h-screen mx-4">
+      <div className="text-center">
+        <h2 className="text-3xl font-custom">{name}</h2>
+        {image && (
+          <Image
+            className="w-80 h-52 mt-4"
+            src={image}
+            height={200}
+            width={200}
+            alt="Project Image"
+          />
+        )}
+        <p className="mt-4 font-custom">{description}</p>
       </div>
-      <div>
+      <div className="mt-4">
         {/* Render technology icons if available */}
         {technology && Array.isArray(technology) && (
-          <div>
+          <div className="flex flex-col">
+            <div>
             <p className="font-custom">Technology used</p>
+            </div>
+            <div className=" flex flex-row">
             {technology.map((iconUrl: string, index: number) => (
               <Image
                 key={index}
@@ -33,13 +42,14 @@ const Experience: React.FC<ProjectCardProps> = () => {
                 alt={`tech-icon-${index}`}
                 height={40}
                 width={40}
-                className="w-5 h-5"
+                className="w-5 h-5 mx-2 flex-row"
               />
+        
             ))}
+            </div>
           </div>
         )}
       </div>
-
     </div>
   );
 };
