80d272145f33450080518946d3932b6e3d036d62
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-25 14:59:36 -0500

    flexed

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -17,13 +17,15 @@ const Experience: React.FC<ProjectCardProps> = () => {
       <div className="text-center">
         <h2 className="text-3xl font-custom">{name}</h2>
         {image && (
-          <Image
-            className="w-80 h-52 mt-4"
-            src={image}
-            height={200}
-            width={200}
-            alt="Project Image"
-          />
+          <div className="mt-4">
+            <Image
+              className="w-[35rem] h-[20rem] mx-auto rounded-md"
+              src={image}
+              height={400}
+              width={400}
+              alt="project-image"
+            />
+          </div>
         )}
         <p className="mt-4 font-custom">{description}</p>
       </div>
@@ -31,21 +33,20 @@ const Experience: React.FC<ProjectCardProps> = () => {
         {/* Render technology icons if available */}
         {technology && Array.isArray(technology) && (
           <div className="flex flex-col">
-            <div>
-            <p className="font-custom">Technology used</p>
+            <div className="text-center">
+              <p className="font-custom">Technology used</p>
             </div>
-            <div className=" flex flex-row">
-            {technology.map((iconUrl: string, index: number) => (
-              <Image
-                key={index}
-                src={iconUrl}
-                alt={`tech-icon-${index}`}
-                height={40}
-                width={40}
-                className="w-5 h-5 mx-2 flex-row"
-              />
-        
-            ))}
+            <div className="flex flex-row">
+              {technology.map((iconUrl: string, index: number) => (
+                <Image
+                  key={index}
+                  src={iconUrl}
+                  alt={`tech-icon-${index}`}
+                  height={100}
+                  width={100}
+                  className="w-10 h-10 mx-2 flex-row"
+                />
+              ))}
             </div>
           </div>
         )}
