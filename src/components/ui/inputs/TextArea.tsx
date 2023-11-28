e1e223615e3cb97c53f7eb7476b2beb12c80380d
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-27 22:43:08 -0500

    const TextArea with props

--- src/components/ui/inputs/TextArea.tsx
+++ src/components/ui/inputs/TextArea.tsx
@@ -1,9 +1,21 @@
 interface TextAreaProps extends React.InputHTMLAttributes<HTMLTextAreaElement> {
     id: string;
-    name: string;
+    firstLastName: string;
     label: string;
     placeholder: string;
     type?: string;
     error?: boolean;
     errorMessage?: string;
+}
+
+const TextArea = ({
+    id,
+    firstLastName,
+    label,
+    placeholder,
+    error,
+    errorMessage,
+    ...props
+}: TextAreaProps) => {
+    
 }
