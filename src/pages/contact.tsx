83b976f6ed333f2837f5658ce477289c2f86158f
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-27 23:59:12 -0500

    cleaned code

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,17 +6,17 @@ interface ContactDarkProps {
 
 const Contact: React.FC<ContactDarkProps> = ({ isDarkMode }) => {
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
-  const [errorMessage, setErrorMessage] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(false);
   async function handleSubmit(event: any) {
     try {
       event.preventDefault();
       const formData = new FormData(event.target);
-  
+
       formData.append("access_key", "755d948f-86c6-4a71-a3a7-beb325a0b965");
-  
+
       const object = Object.fromEntries(formData);
       const json = JSON.stringify(object);
-  
+
       const response = await fetch("https://api.web3forms.com/submit", {
         method: "POST",
         headers: {
@@ -25,9 +25,9 @@ const Contact: React.FC<ContactDarkProps> = ({ isDarkMode }) => {
         },
         body: json,
       });
-  
+
       const result = await response.json();
-  
+
       if (result.success) {
         setSuccessMessage("Form submitted successfully!");
       } else {
@@ -39,7 +39,6 @@ const Contact: React.FC<ContactDarkProps> = ({ isDarkMode }) => {
       setErrorMessage(true);
     }
   }
-  
 
   return (
     <div
@@ -60,27 +59,27 @@ const Contact: React.FC<ContactDarkProps> = ({ isDarkMode }) => {
             name="name"
             required
           />
-          
+
           <label>Email:</label>
           <input
             className="px-2 rounded-lg mb-8 pt-1 pb-1"
             type="email"
             name="email"
             required
-
           />
           <label>Message:</label>
           <textarea
             className="px-2 rounded-lg mb-8 pt-6 pb-6"
             name="message"
             required
-
           ></textarea>
-          <button className="mb-4" type="submit">Submit Form</button>
+          <button className="mb-4" type="submit">
+            Submit Form
+          </button>
         </form>
       )}
-      {successMessage && (<div className="text-green-600">{successMessage}</div>)}
-      {errorMessage && (<div>{errorMessage}</div>)}
+      {successMessage && <div className="text-green-600">{successMessage}</div>}
+      {errorMessage && <div>{errorMessage}</div>}
     </div>
   );
 };
