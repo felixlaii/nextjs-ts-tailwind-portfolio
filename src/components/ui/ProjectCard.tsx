46c1aab517c248576138b3615741db3c6c88eaf8
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-01 15:13:46 -0500

    adjusted height

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -75,8 +75,8 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           </div>
         ) : (
           // Content for lg and xl screens when not hovered
-          <div className="flex flex-col items-center justify-around  w-full h-full">
-            <div className="">
+          <div className="flex flex-col items-center justify-around w-full">
+            <div>
               <h2 className="text-sm lg:text-xl tracking-widest text-brand-light font-semibold mb-4">
                 {name}
               </h2>
@@ -94,7 +94,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         )
       ) : (
         // Content for sm and md screens
-        <div className="flex lg:flex-col align-middle items-center w-[29rem] lg:items-center lg:justify-center md:flex-row md:justify-between justify-between h-1/2">
+        <div className="flex lg:flex-col align-middle items-center w-[29rem] lg:items-center lg:justify-center md:flex-row md:justify-between justify-between ">
           <div className="lg:flex lg:flex-col lg:align-middle lg:items-center lg:mt-4 lg:w-[9rem] lg:h-[9rem] md:w-[10rem] md:h-[10rem] w-[9rem] h-[7.5rem] xl:w-48">
             <Image
               className="rounded-lg ml-[1rem]"
