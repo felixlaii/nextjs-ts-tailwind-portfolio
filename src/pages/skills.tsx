e31ebf743fd7ff2e1009e8da3a3d6aa4aa6b46ca
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-01 18:57:34 -0500

    adjusted mb

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -23,11 +23,14 @@ const Skills: React.FC<SkillsProps> = ({ isDarkMode }) => {
           </h2>
         </div>
         {/* Top Row */}
-        <div className="flex flex-col mb-10 border-2 mt-10 mx-9 pt-7 pb-7 lg:w-[50rem]">
+        <div className="flex flex-col mb-14 border-2 mt-10 mx-9 pt-7 pb-7 lg:w-[50rem]">
           <div>
             <ul className="flex justify-between items-center mx-4 sm:mx-auto">
               {topRowSkills.map((skill, index) => (
-                <li className="flex flex-col items-center text-white" key={skill.image}>
+                <li
+                  className="flex flex-col items-center text-white"
+                  key={skill.image}
+                >
                   <Image
                     width={50}
                     height={50}
@@ -35,7 +38,9 @@ const Skills: React.FC<SkillsProps> = ({ isDarkMode }) => {
                     alt="skills"
                     className="w-8 h-8 sm:w-10 sm:h-10 md:w-18 md:h-18 lg:w-[3rem] lg:h-[3rem] mx-4"
                   />
-                  <p className="text-[0.6rem] font-extralight mt-2 text-center">{skill.description}</p>
+                  <p className="text-[0.6rem] font-extralight mt-2 text-center">
+                    {skill.description}
+                  </p>
                 </li>
               ))}
             </ul>
@@ -45,7 +50,10 @@ const Skills: React.FC<SkillsProps> = ({ isDarkMode }) => {
           <div>
             <ul className="flex justify-between mx-4 sm:mx-auto mt-4">
               {bottomRowSkills.map((skill, index) => (
-                <li className="flex flex-col items-center text-white" key={skill.image}>
+                <li
+                  className="flex flex-col items-center text-white"
+                  key={skill.image}
+                >
                   <Image
                     width={50}
                     height={50}
@@ -53,7 +61,9 @@ const Skills: React.FC<SkillsProps> = ({ isDarkMode }) => {
                     alt="skills"
                     className="w-8 h-8 sm:w-10 sm:h-10 md:w-18 md:h-18 lg:w-[3rem] lg:h-[3rem] mx-4"
                   />
-                  <p className="text-[0.6rem] font-extralight mt-2 text-center">{skill.description}</p>
+                  <p className="text-[0.6rem] font-extralight mt-2 text-center">
+                    {skill.description}
+                  </p>
                 </li>
               ))}
             </ul>
