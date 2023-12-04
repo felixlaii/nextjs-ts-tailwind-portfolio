8a2e5d10dffafb6a2b4293a361da757ad3ee2b3e
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-03 19:00:46 -0500

    added arrow button and changed bg colour

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import { ProjectCardProps } from "@/types/component-types";
 import Image from "next/image";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
+
 
 const Experience: React.FC<ProjectCardProps> = () => {
   const router = useRouter();
@@ -19,22 +22,22 @@ const Experience: React.FC<ProjectCardProps> = () => {
   };
 
   return (
-    <div className="min-h-screen  flex flex-col items-center pt-[8rem] ">
-      <div className="flex justify-start w-full mb-4 pl-8">
-        <button
-          className="border border-brand-dark px-4 py-2 rounded-md text-md font-custom tracking-wide"
+    <div className="h-full flex flex-col items-center pt-[4rem]">
+      <div className="flex flex-row items-center h-[3rem]">
+      <div className="align-middle">
+        <FaArrowLeft
+          className="h-10 w-5"
           onClick={goBack}
-        >
-          Back
-        </button>
+        
+        />
       </div>
-      <h2 className="text-[5rem] ml-4 tracking-widest font-custom text-brand-dark">{name}</h2>
-
+      <h2 className="text-[1.5rem] ml-4 tracking-widest font-custom text-brand-dark">{name}</h2>
+      </div>
       <div className="bg-brand-light w-full text-center">
         {image && (
           <div className="mt-4">
             <Image
-              className="w-[25rem] h-[20rem] mx-auto rounded-md"
+              className="w-[20rem] h-[20rem] mx-auto rounded-md"
               src={image}
               height={400}
               width={400}
@@ -42,13 +45,13 @@ const Experience: React.FC<ProjectCardProps> = () => {
             />
           </div>
         )}
-        <p className="mt-4 max-w-[800px] text-zinc-200 tracking-widest mx-auto text-center font-custom pb-4">{longDescription}</p>
+        <p className="mt-4 max-w-[800px] text-zinc-200 tracking-widest mx-10 text-center font-custom pb-4">{longDescription}</p>
       </div>
       <div className="mt-4">
         {technology && Array.isArray(technology) && (
           <div className="flex flex-col">
             <div className="text-center">
-              <p className="font-custom text-lg mt-4 text-brand-dark tracking-wide">Technology used</p>
+              <p className="font-custom text-lg mt-4 mb-5 text-brand-dark tracking-wide">Technology used</p>
             </div>
             <div className="flex flex-row">
               {technology.map((iconUrl: string, index: number) => (
@@ -65,13 +68,13 @@ const Experience: React.FC<ProjectCardProps> = () => {
           </div>
         )}
       </div>
-      <div>
+      <div className="bg-brand-light w-full mt-9">
         {(githubUrl || deployedUrl) && (
-          <div className="flex flex-col">
+          <div className="flex flex-col items-center">
             <div className="text-center mt-6">
-              <p className="font-custom text-lg text-brand-dark tracking-wide">View My Work</p>
+              <p className="font-custom text-lg mb-5 text-brand-dark tracking-wide">View My Work</p>
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row mb-9">
               {githubUrl && (
                 <a
                   className="border border-brand-dark px-10 mx-4 font-custom tracking-wider rounded-sm hover:text-brand-light bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
