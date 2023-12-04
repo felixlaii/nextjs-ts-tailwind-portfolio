19383b49aa6ca23be0a57e19a09942899c7eb016
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-03 19:13:36 -0500

    created function and prev and next button

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,5 +7,16 @@ import { CarouselProps } from "@/types/component-types";
 export default function Carousel({ width, height, items}: CarouselProps) {
     const [activeIndex, setActiveIndex] = useState<number>(0);
 
+    function handleNextItemBtn() {
+        setActiveIndex((prev) => {
+          return prev + 1 < items.length ? prev + 1 : prev;
+        });
+      }
+    
+      function handlePrevItemBtn() {
+        setActiveIndex((prev) => {
+          return prev - 1 >= 0 ? prev - 1 : prev;
+        });
+      }
 
 }
