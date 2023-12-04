88e2a779512331b59f3a0ede83dd917a20156658
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-03 19:07:58 -0500

    created dots

--- src/components/ui/CarouselIndicator.tsx
+++ src/components/ui/CarouselIndicator.tsx
@@ -5,8 +5,19 @@ export default function CarouselIndicator({ activeIndex, length, maxIndicatorVis
     const maxIndicator = length > maxIndicatorVisible ? maxIndicatorVisible : length;
 
     return (
-        <div>
-            
-        </div>
+        <div className="carousel-indicator">
+      {Array.from(Array(maxIndicator), (_, index) => {
+        return (
+          <div
+            key={index}
+            className={`carousel-indicator-dots
+            ${activeIndex === index ? "w-4 opacity-100" : "w-2 bg-gray-400"}`}
+            onClick={() => {
+              onSetActiveIndex(index);
+            }}
+          ></div>
+        );
+      })}
+    </div>
     )
 }
