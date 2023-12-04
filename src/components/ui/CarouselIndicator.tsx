6133a4809cab75174ff66cb6d1c3533e9a715897
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-03 19:07:32 -0500

    created carousel indicator function with props

--- src/components/ui/CarouselIndicator.tsx
+++ src/components/ui/CarouselIndicator.tsx
@@ -1,1 +1,12 @@
 import React from "react";
+import { CarouselIndicatorProps } from "@/types/component-types";
+
+export default function CarouselIndicator({ activeIndex, length, maxIndicatorVisible = 5, onSetActiveIndex,} : CarouselIndicatorProps) {
+    const maxIndicator = length > maxIndicatorVisible ? maxIndicatorVisible : length;
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
