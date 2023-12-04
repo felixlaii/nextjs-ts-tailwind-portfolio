edeaf963456a57019f4ab66dddaf31ea176cfe05
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-03 19:10:53 -0500

    set state, and offset, direction and absoffset

--- src/components/ui/CarouselItem.tsx
+++ src/components/ui/CarouselItem.tsx
@@ -2,5 +2,9 @@ import React, { useState } from 'react';
 import { CarouselCardProps } from '@/types/component-types';
 
 export function CarouselItem({ index, activeIndex, children}: CarouselCardProps) {
-    
+    const [scaled, setScaled] = useState<Boolean>(false);
+
+    const offset = (index - activeIndex) / 4;
+    const direction = Math.sign(index - activeIndex);
+    const absOffset = Math.abs(offset);
 }
