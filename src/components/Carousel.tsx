903aeb7a6c6c5f6efa7c902067bd51f0c7c93099
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-03 19:14:03 -0500

    created carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,19 +4,56 @@ import CarouselIndicator from "./ui/CarouselIndicator";
 import { IoIosArrowBack } from "react-icons/io";
 import { CarouselProps } from "@/types/component-types";
 
-export default function Carousel({ width, height, items}: CarouselProps) {
-    const [activeIndex, setActiveIndex] = useState<number>(0);
+export default function Carousel({ width, height, items }: CarouselProps) {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
 
-    function handleNextItemBtn() {
-        setActiveIndex((prev) => {
-          return prev + 1 < items.length ? prev + 1 : prev;
-        });
-      }
-    
-      function handlePrevItemBtn() {
-        setActiveIndex((prev) => {
-          return prev - 1 >= 0 ? prev - 1 : prev;
-        });
-      }
+  function handleNextItemBtn() {
+    setActiveIndex((prev) => {
+      return prev + 1 < items.length ? prev + 1 : prev;
+    });
+  }
 
+  function handlePrevItemBtn() {
+    setActiveIndex((prev) => {
+      return prev - 1 >= 0 ? prev - 1 : prev;
+    });
+  }
+
+  return (
+    <div className="carousel-container">
+      {activeIndex > 0 && (
+        <button
+          className="carousel-btn-switch-card-left carousel-btn-switch-card"
+          onClick={handlePrevItemBtn}
+        >
+          <IoIosArrowBack />
+        </button>
+      )}
+      {items?.map((item, index) => (
+        <CarouselItem key={index} index={index} activeIndex={activeIndex}>
+          {item}
+        </CarouselItem>
+      ))}
+      {activeIndex < items.length - 1 && (
+        <button
+          className="carousel-btn-switch-card-right carousel-btn-switch-card"
+          onClick={handleNextItemBtn}
+        >
+          <IoIosArrowBack
+            style={{
+              transform: "rotate(180deg)",
+            }}
+          />
+        </button>
+      )}
+
+      <CarouselIndicator
+        activeIndex={activeIndex}
+        length={items.length}
+        onSetActiveIndex={(activeIndex) => {
+          setActiveIndex(activeIndex);
+        }}
+      />
+    </div>
+  );
 }
