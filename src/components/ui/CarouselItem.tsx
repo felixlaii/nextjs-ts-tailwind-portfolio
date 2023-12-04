e8d958a1025d29bead21ccf160ad95d8de5cc2cd
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-03 19:11:15 -0500

    css properties

--- src/components/ui/CarouselItem.tsx
+++ src/components/ui/CarouselItem.tsx
@@ -7,4 +7,19 @@ export function CarouselItem({ index, activeIndex, children}: CarouselCardProps)
     const offset = (index - activeIndex) / 4;
     const direction = Math.sign(index - activeIndex);
     const absOffset = Math.abs(offset);
+
+    const cssTransformProperties = `
+        rotateY(calc(${offset} * 55deg))
+        scaleY(calc(1 +  ${absOffset}  * -0.5))
+        translateX(calc( ${direction} * -3.5rem))
+        translateZ(calc( ${absOffset} * -35rem))
+        scale(${scaled && index === activeIndex ? 6.5 : 1})
+       `;
+
+  const cssOpacity = `
+        ${Math.abs(index - activeIndex) >= 3 ? '0' : '1'}`;
+
+  const cssDisplay = `
+        ${Math.abs(index - activeIndex) >= 3 ? 'none' : 'block'},
+  `;
 }
