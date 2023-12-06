83d3d60623771ae1ba6dba59cd8035db2ecf2714
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-05 19:06:23 -0500

    added carousel array and indicator

--- src/components/ui/ImageSlider.tsx
+++ src/components/ui/ImageSlider.tsx
@@ -1,10 +1,18 @@
 import { useState, useRef, useEffect } from "react";
 import { ProjectCardProps } from "@/types/component-types";
-const ImageSlider: React.FC<ProjectCardProps> = ({ carousel }) => {
-return (
+
+const ImageSlider: React.FC<ProjectCardProps> = ({ carousel = [] }) => {
+    const indicatorWidthPercent = carousel.length > 0 ? 100 / carousel.length : 100;
+
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+    const sliderRef = useRef<HTMLDivElement>(null);
+
+    return (
     <div>
 
     </div>
+
+
 )
 }
 
