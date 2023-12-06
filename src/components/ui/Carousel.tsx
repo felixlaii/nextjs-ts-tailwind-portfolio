786964b39ec8e154e4f2c72aa164080aba915b85
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-05 19:41:15 -0500

    state and scroll to image and refs

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,11 +1,29 @@
 import ImageSlider from "./ImageSlider";
 import clsx from "clsx";
 import { createRef, useState } from "react";
+import { ProjectCardProps } from "@/types/component-types";
+const Carousel: React.FC<ProjectCardProps> = ({ carousel = [] }) => {
+  const [currentImage, setCurrentImage] = useState<number>(0);
 
-const Carousel: React.FC = () => {
-  return (
-    <div></div>
-  )
-}
+  const refs = carousel.reduce((acc: any, val, i) => {
+    acc[i] = createRef();
+    return acc;
+  }, {});
+
+
+  const scrollToImage = (i: number) => {
+    // Set the index of the image we want to see next
+    setCurrentImage(i);
+    refs[i].current.scrollIntoView({
+      //     Defines the transition animation.
+      behavior: "smooth",
+      //      Defines vertical alignment.
+      block: "nearest",
+      //      Defines horizontal alignment.
+      inline: "start",
+    });
+  };
+  return <div></div>;
+};
 
 export default Carousel;
