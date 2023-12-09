7329192c7e66eda8dfecbe24a21991fad702ed87
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-09 15:42:39 -0500

    stopping animation

--- src/components/ui/FramerCarousel.tsx
+++ src/components/ui/FramerCarousel.tsx
@@ -1,48 +1,47 @@
-import { MouseEvent as ReactMouseEvent, useRef, useState } from "react"
-import Link from "next/link"
-import { motion, useMotionValue, useSpring, type PanInfo } from "framer-motion"
-import { MoveLeft, MoveRight } from "lucide-react"
+import { MouseEvent as ReactMouseEvent, useRef, useState } from "react";
+import Link from "next/link";
+import { motion, useMotionValue, useSpring, type PanInfo } from "framer-motion";
+import { MoveLeft, MoveRight } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const START_INDEX = 1
-const DRAG_THRESHOLD = 150
-const FALLBACK_WIDTH = 509
+const START_INDEX = 1;
+const DRAG_THRESHOLD = 150;
+const FALLBACK_WIDTH = 509;
 
-const CURSOR_SIZE = 80
+const CURSOR_SIZE = 80;
 
 interface FramerCarouselProps {
-    carousel: string[];
-  }
+  carousel: string[];
+}
 
 const FramerCarousel: React.FC<FramerCarouselProps> = ({ carousel = [] }) => {
-    const containerRef = useRef<HTMLUListElement>(null)
-    const itemsRef = useRef<(HTMLLIElement | null)[]>([])
-    const [activeSlide, setActiveSlide] = useState(START_INDEX)
-    const canScrollPrev = activeSlide > 0
-    const canScrollNext = activeSlide < carousel.length - 1
-    const offsetX = useMotionValue(0)
-    const animatedX = useSpring(offsetX, {
-      damping: 20,
-      stiffness: 150,
-    })
+  const containerRef = useRef<HTMLUListElement>(null);
+  const itemsRef = useRef<(HTMLLIElement | null)[]>([]);
+  const [activeSlide, setActiveSlide] = useState(START_INDEX);
+  const canScrollPrev = activeSlide > 0;
+  const canScrollNext = activeSlide < carousel.length - 1;
+  const offsetX = useMotionValue(0);
+  const animatedX = useSpring(offsetX, {
+    damping: 20,
+    stiffness: 150,
+  });
 
-    const [isDragging, setIsDragging] = useState(false)
+  const [isDragging, setIsDragging] = useState(false);
 
-    function handleDragSnap(
-        _: MouseEvent,
-        { offset: { x: dragOffset } }: PanInfo,
-    ) {
-        //reset drag state
-        setIsDragging(false)
-        containerRef.current?.removeAttribute("data-dragging")
-    }
+  function handleDragSnap(
+    _: MouseEvent,
+    { offset: { x: dragOffset } }: PanInfo
+  ) {
+    //reset drag state
+    setIsDragging(false);
+    containerRef.current?.removeAttribute("data-dragging");
 
-    return (
-        <div>
+    //stop drag animation (rest velocity)
+    animatedX.stop();
+  }
 
-        </div>
-    )
-}
+  return <div></div>;
+};
 
 export default FramerCarousel;
