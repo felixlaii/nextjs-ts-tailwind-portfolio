88e60ed8ffbf7ff3fc569087411bd4b0d3064ce9
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-09 15:36:37 -0500

    Update FramerCarousel.tsx

--- src/components/ui/FramerCarousel.tsx
+++ src/components/ui/FramerCarousel.tsx
@@ -3,7 +3,13 @@ import Link from "next/link"
 import { motion, useMotionValue, useSpring, type PanInfo } from "framer-motion"
 import { MoveLeft, MoveRight } from "lucide-react"
 
-import { cn } from "~/lib/utils"
+import { cn } from "@/lib/utils"
+
+const START_INDEX = 1
+const DRAG_THRESHOLD = 150
+const FALLBACK_WIDTH = 509
+
+const CURSOR_SIZE = 80
 
 export default function FramerCarousel() {
     return (
