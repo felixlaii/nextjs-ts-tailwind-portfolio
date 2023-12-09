38d91772840b4d9d15bc2351423dfe4e7b97182f
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-09 15:36:12 -0500

    imported files for FramerCarousel

--- src/components/ui/FramerCarousel.tsx
+++ src/components/ui/FramerCarousel.tsx
@@ -1,4 +1,9 @@
+import { MouseEvent as ReactMouseEvent, useRef, useState } from "react"
+import Link from "next/link"
+import { motion, useMotionValue, useSpring, type PanInfo } from "framer-motion"
+import { MoveLeft, MoveRight } from "lucide-react"
 
+import { cn } from "~/lib/utils"
 
 export default function FramerCarousel() {
     return (
