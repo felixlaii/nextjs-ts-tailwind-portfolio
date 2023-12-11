d9b55a9ac9f38e0c055da1e4037215f6a69aa1c4
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-11 18:37:15 -0500

    create pagetransition and added props

---
+++ src/components/ui/PageTransition.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+
+const PageTransition = ({ Component, pageProps }: AppProps) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PageTransition;
