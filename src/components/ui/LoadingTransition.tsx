33534fe567d7fa6df80dde75c99d04abe688a7a9
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-11 18:10:14 -0500

    styled loading container

--- src/components/ui/LoadingTransition.tsx
+++ src/components/ui/LoadingTransition.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const LoadingTransition: React.FC = () => {
 
     return (
-        <div>
-
+<div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-white z-50">
         </div>
     )
 }
