c909fc5fd2fb63f52264fa97b1b1cf166dd6e989
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-14 20:10:54 -0500

    Update success.tsx

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,15 @@
 export default function Success() {
-  return <div>Form successfully submitted!</div>;
+  return (
+    <div className="min-h-screen flex pt-20 justify-center">
+      <div className="text-center ml-5 mr-5">
+        <h1 className="text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] tracking-widest pb-5 font-custom text-brand-dark">
+          Lets Talk...
+        </h1>
+        <p className="text-[3rem] w-[calc(10% - 10px)] text-brand-light font-custom tracking-widest">
+          Your submission has been successful! Thank you for connecting, I will
+          reach out as soon as possible!
+        </p>
+      </div>
+    </div>
+  );
 }
