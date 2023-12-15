5e4897221489bd86b06f0c04ffd8c2be5a08cfcc
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-14 20:18:34 -0500

    Update success.tsx

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -8,12 +8,20 @@ export default function Success() {
     router.back();
   };
   return (
-    <div className="min-h-screen flex pt-20 justify-center">
-      <div className="text-center ml-5 mr-5">
+    <div className="flex flex-col min-h-screen pt-[5rem]">
+      <div className="flex flex-row items-center mx-auto">
+        <div className="align-middle">
+          <FaArrowLeft
+            className="h-10 w-5 cursor-pointer mr-5"
+            onClick={goBack}
+          />
+        </div>
         <h1 className="text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] tracking-widest pb-5 font-custom text-brand-dark">
           Lets Talk...
         </h1>
-        <p className="text-[3rem] w-[calc(10% - 10px)] text-brand-light font-custom tracking-widest">
+      </div>
+      <div>
+        <p className="text-[3rem] text-center text-brand-light font-custom tracking-widest">
           Your submission has been successful! Thank you for connecting, I will
           reach out as soon as possible!
         </p>
