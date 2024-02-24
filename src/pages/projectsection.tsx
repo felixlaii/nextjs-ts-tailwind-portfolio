96049d8270515810e74d708ab7939c596a980fee
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-23 20:51:29 -0500

    create filter project

--- src/pages/projectsection.tsx
+++ src/pages/projectsection.tsx
@@ -11,4 +11,40 @@ const ProjectSection: React.FC<ProjectSetUpProps> = ({ isDarkMode }) => {
   const [selectedCategory, setSelectedCategory] = useState("all");
 
   const router = useRouter();
+
+  const filteredProjects =
+    selectedCategory === "all"
+      ? ProjectsData
+      : ProjectsData.filter((project) => project.category === selectedCategory);
+
+  const handleProjectClick = (projectId: string) => {
+    console.log(`Clicked project with ID: ${projectId}`);
+
+    const selectedProject = ProjectsData.find(
+      (project) => project.id === projectId
+    );
+
+    if (selectedProject) {
+      router.push(
+        {
+          pathname: "/experience",
+          query: {
+            id: projectId,
+            name: selectedProject.name,
+            description: selectedProject.description,
+            image: selectedProject.image,
+            technology: selectedProject.technology,
+            longDescription: selectedProject.longDescription,
+            deployedUrl: selectedProject.deployedUrl,
+            githubUrl: selectedProject.githubUrl,
+            carousel: selectedProject.carousel,
+          },
+        },
+        `/experience#${selectedProject.id}`
+      );
+    }
+  };
+
+  return <div></div>;
 };
+export default ProjectSection;
