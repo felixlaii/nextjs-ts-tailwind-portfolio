615710a5845c9666444c21e51d03fe25e042e425
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-25 17:29:19 -0500

    deleted file

--- src/components/ui/ProjectSetUp.tsx
+++ src/components/ui/ProjectSetUp.tsx
@@ -26,7 +26,7 @@ const ProjectSetUp: React.FC<ProjectCardProps> = ({
     <motion.div
       className={`flex flex-col justify-center shadow-2xl pt-8 pb-6 pl-10 pr-10 cursor-pointer ${
         isDarkMode ? "bg-brand-light bg-cover" : "bg-brand-base bg-cover"
-      } box-border rounded-sm h-[14rem] w-[25rem] md:w-[40rem] md:h-[25rem] lg:w-[43rem] lg:h-[20rem] xl:w-[44rem] xl:h-[21rem] md:hover:scale-110 lg:hover:scale-110 xl:hover:scale-110`}
+      } box-border rounded-sm h-[13rem] w-[23.7rem] md:w-[40rem] md:h-[25rem] lg:w-[43rem] lg:h-[20rem] xl:w-[44rem] xl:h-[21rem] md:hover:scale-110 lg:hover:scale-110 xl:hover:scale-110`}
       data-aos={index % 2 === 0 ? "slide-left" : "slide-right"}
       whileHover={{
         scale: [0, 1.1], // Initial scale 1 for small screens, 1.1 for md, lg, and xl screens
@@ -48,15 +48,15 @@ const ProjectSetUp: React.FC<ProjectCardProps> = ({
                 key={index}
                 src={iconUrl}
                 alt={`tech-icon-${index}`}
-                className="w-8 h-8 mb-4 px-1"
+                className="w-7 h-7 mb-4 px-1"
               />
             ))}
           </div>
           <div className="">
             <Image
               className="rounded-lg object-center"
-              width={200}
-              height={200}
+              width={150}
+              height={150}
               src={image}
               alt="project logo"
             />
@@ -64,7 +64,7 @@ const ProjectSetUp: React.FC<ProjectCardProps> = ({
         </div>
         <div className="flex items-center ml-20 w-1/2">
           <p
-            className={`text-sm lg:text-lg font-extralight ${
+            className={`text-xs lg:text-lg font-extralight ${
               isDarkMode
                 ? isHovered
                   ? "text-brand-darkest"
