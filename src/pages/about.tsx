e4e9d7e2d9c76722d4f8bb21148c5d01bfe5164a
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-25 20:26:06 -0500

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -56,7 +56,7 @@ const About: React.FC<AboutDarkProps> = ({ isDarkMode }) => {
         </div>
         <button
           data-aos="fade-up"
-          className={`flex flex-row border-2 border-brand-base items-center px-5 py-2.5 text-center me-2 mb-9 mt-4 font-custom tracking-wider rounded-md hover:text-brand-lightest bg-gradient-to-r from-brand-light/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out ${
+          className={`flex flex-row border-double border-4 border-brand-base items-center px-5 py-2.5 text-center me-2 mb-9 mt-4 font-custom tracking-wider rounded-md hover:text-brand-lightest bg-gradient-to-r from-brand-light/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out ${
             isDarkMode
               ? "text-brand-lightest border-brand-dark"
               : "text-brand-dark border-brand-base"
