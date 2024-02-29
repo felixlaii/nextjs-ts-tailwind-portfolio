fba84424c80e94952c5b1d55f05c0b3172692857
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-29 17:47:28 -0500

    added useEffect for dark mode

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,4 +1,19 @@
+import { useState, useEffect } from "react";
+
 const ThemeToggle = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") setDarkMode(true);
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    }
+  }, [darkMode]);
+
   return <div></div>;
 };
 
