2381f9523a610867b1fe46829a94f839ce07b38c
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-29 17:50:49 -0500

    Update ThemeToggle.tsx

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -18,7 +18,33 @@ const ThemeToggle = () => {
     }
   }, [darkMode]);
 
-  return <div></div>;
+  return (
+    <div className="flex items-center ml-4 cursor-pointer">
+      <label className="flex items-center cursor-pointer">
+        <div className="relative flex items-center">
+          <input
+            type="checkbox"
+            className="hidden"
+            onClick={() => setDarkMode(!darkMode)}
+          />
+          <div className="toggle-track w-8 h-4 bg-gray-400 rounded-full shadow-inner relative">
+            <div
+              className={`toggle-thumb absolute w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+                darkMode ? "translate-x-full" : "translate-x-0"
+              }`}
+            ></div>
+          </div>
+          <p
+            className={`font-custom tracking-widest ml-2 ${
+              darkMode ? "text-brand-lighter" : "text-brand-light"
+            }`}
+          >
+            {darkMode ? "LHT" : "DRK"}
+          </p>
+        </div>
+      </label>
+    </div>
+  );
 };
 
 export default ThemeToggle;
