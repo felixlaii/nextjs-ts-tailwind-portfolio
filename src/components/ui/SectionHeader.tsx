9596f62412f85f7c7742a6c361f7b062cf405fc2
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-29 16:02:42 -0500

    added header

--- src/components/ui/SectionHeader.tsx
+++ src/components/ui/SectionHeader.tsx
@@ -1,3 +1,11 @@
 export interface SectionHeaderProps {
   title: string;
 }
+
+const SectionHeader: React.FC<SectionHeaderProps> = ({ title }) => {
+  return (
+    <>
+      <h2 className="mt-9 text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] tracking-widest"></h2>
+    </>
+  );
+};
