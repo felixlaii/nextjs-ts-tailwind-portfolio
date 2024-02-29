9b2c3beeb4a6c5e450fc49ab23f2e913ffb95869
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-29 17:01:12 -0500

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -32,7 +32,7 @@ const About: React.FC<AboutDarkProps> = ({ isDarkMode }) => {
       }`}
     >
       <div className="flex justify-center flex-col items-center font-custom">
-        <div data-aos="fade-down" className="justify-center">
+        <div data-aos="zoom-in-down" className="justify-center">
           <h1
             className={`mt-9 text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] tracking-widest ${
               isDarkMode ? "text-brand-light" : "text-brand-base"
