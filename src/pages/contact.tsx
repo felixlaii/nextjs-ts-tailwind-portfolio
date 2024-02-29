571ab015bf4162f2cd631b32f6eb3ce9ad70c879
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-29 16:16:09 -0500

    added aos init

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,6 +4,12 @@ import "aos/dist/aos.css";
 import { ContactDarkProps } from "@/types/component-types";
 
 const Contact: React.FC<ContactDarkProps> = ({ isDarkMode }) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+      once: true,
+    });
+  }, []);
   return (
     <div
       className={`flex flex-col items-center font-custom mt-10 ${
