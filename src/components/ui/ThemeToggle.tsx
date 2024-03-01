1cba01f90775425514e749279b13f182a128d9b7
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-02-29 20:15:33 -0500

    added moon

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { FaMoon } from "react-icons/fa";
+import { BsSunFill } from "react-icons/bs";
 
 const ThemeToggle = () => {
   const [darkMode, setDarkMode] = useState(true);
@@ -19,30 +21,40 @@ const ThemeToggle = () => {
   }, [darkMode]);
 
   return (
-    <div className="flex items-center ml-4 cursor-pointer">
-      <label className="flex items-center cursor-pointer">
-        <div className="relative flex items-center">
-          <input
-            type="checkbox"
-            className="hidden"
-            onClick={() => setDarkMode(!darkMode)}
-          />
-          <div className="toggle-track w-8 h-4 bg-gray-400 rounded-full shadow-inner relative">
-            <div
-              className={`toggle-thumb absolute w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
-                darkMode ? "translate-x-full" : "translate-x-0"
-              }`}
-            ></div>
-          </div>
-          <p
-            className={`font-custom tracking-widest ml-2 ${
-              darkMode ? "text-brand-lighter" : "text-brand-light"
-            }`}
-          >
-            {darkMode ? "LHT" : "DRK"}
-          </p>
-        </div>
-      </label>
+    // <div className="flex items-center ml-4 cursor-pointer">
+    //   <label className="flex items-center cursor-pointer">
+    //     <div className="relative flex items-center">
+    //       <input
+    //         type="checkbox"
+    //         className="hidden"
+    //         onClick={() => setDarkMode(!darkMode)}
+    //       />
+    //       <div className="toggle-track w-8 h-4 bg-gray-400 rounded-full shadow-inner relative">
+    //         <div
+    //           className={`toggle-thumb absolute w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+    //             darkMode ? "translate-x-full" : "translate-x-0"
+    //           }`}
+    //         ></div>
+    //       </div>
+    //       <p
+    //         className={`font-custom tracking-widest ml-2 ${
+    //           darkMode ? "text-brand-lighter" : "text-brand-light"
+    //         }`}
+    //       >
+    //         {darkMode ? "LHT" : "DRK"}
+    //       </p>
+    //     </div>
+    //   </label>
+    // </div>
+    <div
+      className="relative w-16 h-8 flex items-center dark:bg-gray-900 bg-teal-500 cursor-pointer rounded-full p-1"
+      onClick={() => setDarkMode(!darkMode)}
+    >
+      <FaMoon className="text-white" size={18} />
+      <div
+        className="absolute bg-white dark:bg-brand-light w-6 h-6 rounded-full shadow-md transform transition-transform duration-300"
+        style={darkMode ? { left: "2px" } : { right: "2px" }}
+      ></div>
     </div>
   );
 };
