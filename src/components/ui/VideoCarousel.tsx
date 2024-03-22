d9e19252c6de417ab2ae4903ab592b09deb624ba
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-22 18:38:40 -0400

    Update VideoCarousel.tsx

--- src/components/ui/VideoCarousel.tsx
+++ src/components/ui/VideoCarousel.tsx
@@ -9,6 +9,7 @@ interface VideoCarouselProps {
 
 const START_INDEX = 1;
 const DRAG_THRESHOLD = 150;
+const FALLBACK_WIDTH = 509;
 
 const CURSOR_SIZE = 80;
 
@@ -16,8 +17,12 @@ const VideoCarousel: React.FC<VideoCarouselProps> = ({
   videoCarousel = [],
 }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const itemsRef = useRef<(HTMLLIElement | null)[]>([]);
 
   const [activeSlide, setActiveSlide] = useState(START_INDEX);
+  const canScrollPrev = activeSlide > 0;
+  const canScrollNext = activeSlide < videoCarousel.length - 1;
+
   const offsetX = useMotionValue(0);
   const animatedX = useSpring(offsetX, {
     damping: 20,
@@ -35,6 +40,52 @@ const VideoCarousel: React.FC<VideoCarouselProps> = ({
     animatedX.stop();
 
     const currentOffset = offsetX.get();
+
+    if (
+      Math.abs(dragOffset) < DRAG_THRESHOLD ||
+      (!canScrollPrev && dragOffset > 0) ||
+      (!canScrollNext && dragOffset < 0)
+    ) {
+      animatedX.set(currentOffset);
+      return;
+    }
+
+    let offsetWidth = 0;
+
+    for (
+      let i = activeSlide;
+      dragOffset > 0 ? i >= 0 : i < videoCarousel.length;
+      dragOffset > 0 ? i-- : i++
+    ) {
+      const item = itemsRef.current[i];
+      if (item === null) continue;
+      const itemOffset = item.offsetWidth;
+      const prevItemWidth =
+        itemsRef.current[i - 1]?.offsetWidth ?? FALLBACK_WIDTH;
+      const nextItemWidth =
+        itemsRef.current[i + 1]?.offsetWidth ?? FALLBACK_WIDTH;
+
+      if (
+        (dragOffset > 0 && dragOffset > offsetWidth + itemOffset && i > 1) ||
+        (dragOffset < 0 &&
+          dragOffset < offsetWidth + -itemOffset &&
+          i < videoCarousel.length - 2)
+      ) {
+        dragOffset > 0
+          ? (offsetWidth += prevItemWidth)
+          : (offsetWidth -= nextItemWidth);
+        continue;
+      }
+
+      if (dragOffset > 0) {
+        offsetX.set(currentOffset + offsetWidth + prevItemWidth);
+        setActiveSlide(i - 1);
+      } else {
+        offsetX.set(currentOffset + offsetWidth - nextItemWidth);
+        setActiveSlide(i + 1);
+      }
+      break;
+    }
   }
 
   return <div></div>;
