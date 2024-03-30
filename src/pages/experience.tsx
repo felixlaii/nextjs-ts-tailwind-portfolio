310cced99f0aa4ca028141a84af759276d5d8cb8
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-30 17:45:12 -0400

    changed the page set up for experience page

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -43,8 +43,8 @@ const Experience: React.FC<ProjectCardProps> = () => {
               <button
                 className={`mr-4 ${
                   selectedCategory === "image"
-                    ? "font-bold font-custom text-brand-lightish dark:text-brand-darkMode text-[1.2rem] tracking-[0.2rem]"
-                    : "font-bold font-custom text-brand-darkMode dark:text-brand-cardbg text-[1rem] tracking-widest"
+                    ? "font-bold font-custom text-brand-altDarkMode dark:text-brand-darkMode text-[1.2rem] tracking-[0.2rem]"
+                    : "font-bold font-custom text-brand-cardbg dark:text-brand-cardbg text-[1rem] tracking-widest"
                 }`}
                 onClick={() => setSelectedCategory("image")}
               >
@@ -53,8 +53,8 @@ const Experience: React.FC<ProjectCardProps> = () => {
               <button
                 className={`${
                   selectedCategory === "video"
-                    ? "font-bold font-custom text-brand-lightish dark:text-brand-darkMode text-[1.2rem] tracking-[0.2rem]"
-                    : "font-bold font-custom text-brand-darkMode dark:text-brand-cardbg text-[1rem] tracking-widest"
+                    ? "font-bold font-custom text-brand-altDarkMode dark:text-brand-darkMode text-[1.2rem] tracking-[0.2rem]"
+                    : "font-bold font-custom text-brand-cardbg dark:text-brand-cardbg text-[1rem] tracking-widest"
                 }`}
                 onClick={() => setSelectedCategory("video")}
               >
@@ -64,8 +64,8 @@ const Experience: React.FC<ProjectCardProps> = () => {
           )}
       </div>
       {selectedCategory === "image" && (
-        <div className="bg-brand-darkMode dark:bg-brand-base w-full pt-9">
-          <div className="mt-4 pb-9">
+        <div className="bg-brand-darkMode dark:bg-brand-base w-full">
+          <div className=" pb-4">
             {carousel && carousel.length > 0 ? (
               <Carousel carousel={carousel} />
             ) : (
@@ -78,17 +78,62 @@ const Experience: React.FC<ProjectCardProps> = () => {
               />
             )}
           </div>
+          {(githubUrl || deployedUrl) && (
+            <div className="flex flex-col items-center text-center">
+              <div className="flex flex-row mb-9">
+                {githubUrl && (
+                  <a
+                    className="border-double border-4 border-brand-lightMode dark:border-brand-darkMode hover:border-brand-darkest px-10 mx-4 font-custom tracking-wider rounded-sm hover:text-brand-lightish dark:hover:text-brand-cardbg bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+                    href={githubUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </a>
+                )}
+                {deployedUrl && (
+                  <a
+                    className="border-double border-4 border-brand-lightMode dark:border-brand-darkMode hover:border-brand-darkest px-10 mx-4 font-custom tracking-wider rounded-sm hover:text-brand-lightish dark:hover:text-brand-cardbg bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+                    href={deployedUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Deployed
+                  </a>
+                )}
+              </div>
+            </div>
+          )}
           <div className="bg-brand-base dark:bg-brand-darkMode w-full mx-auto pt-7 pb-7 px-9">
-            <p className="mt-4 max-w-[900px] text-4 sm:text-[1.5rem] md:text-[2rem] lg:text-[1.5rem] text-brand-dark tracking-widest mx-auto text-center font-custom pb-4">
+            <p className="mt-4 max-w-[900px] text-4 sm:text-[1.5rem] md:text-[2rem] lg:text-[1.3rem] text-brand-darkMode dark:text-brand-lightMode tracking-widest mx-auto text-center font-custom pb-4">
               {longDescription}
             </p>
           </div>
+          {technology && Array.isArray(technology) && (
+            <div className="bg-brand-base dark:bg-brand-darkMode flex flex-col items-center text-center mx-auto">
+              <h3 className="font-custom text-[1.5rem] sm:text-[1.8rem] md:text-[2.3rem] lg:text-[2.3rem] mb-5 text-brand-darkMode dark:text-brand-base tracking-widest">
+                Technology used
+              </h3>
+              <div className="flex flex-row">
+                {technology.map((iconUrl: string, index: number) => (
+                  <Image
+                    key={index}
+                    src={iconUrl}
+                    alt={`tech-icon-${index}`}
+                    height={100}
+                    width={100}
+                    className="w-7 h-7 mx-5 flex-row mb-10"
+                  />
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       )}
 
       {selectedCategory === "video" && (
-        <div className="bg-brand-darkMode dark:bg-brand-base w-full pt-9">
-          <div className="mt-4 pb-9">
+        <div className="bg-brand-darkMode dark:bg-brand-base w-full">
+          <div className=" pb-4">
             {videoCarousel && videoCarousel.length > 0 ? (
               <VideoCarousel videoCarousel={videoCarousel} />
             ) : (
@@ -101,46 +146,12 @@ const Experience: React.FC<ProjectCardProps> = () => {
               />
             )}
           </div>
-          <div className="bg-brand-base dark:bg-brand-darkMode w-full mx-auto pt-7 pb-7 px-9">
-            <p className="mt-4 max-w-[900px] text-4 sm:text-[1.5rem] md:text-[2rem] lg:text-[1.5rem] text-brand-darkMode tracking-widest mx-auto text-center font-custom pb-4">
-              {longDescription}
-            </p>
-          </div>
-        </div>
-      )}
-      <div className="flex flex-row w-full">
-        <div className="flex flex-col items-center bg-brand-darkMode dark:bg-brand-base w-full mx-auto pt-7 pb-7">
-          {technology && Array.isArray(technology) && (
-            <div className="flex flex-col items-center text-center mx-auto">
-              <h3 className="font-custom text-[1.5rem] sm:text-[1.8rem] md:text-[2.3rem] lg:text-[2.3rem] mt-4 mb-5 text-brand-dark tracking-widest">
-                Technology used
-              </h3>
-              <div className="flex flex-row">
-                {technology.map((iconUrl: string, index: number) => (
-                  <Image
-                    key={index}
-                    src={iconUrl}
-                    alt={`tech-icon-${index}`}
-                    height={100}
-                    width={100}
-                    className="w-10 h-10 mx-2 flex-row mx-5"
-                  />
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center bg-brand-darkMode dark:bg-brand-base w-full mx-auto pt-9">
           {(githubUrl || deployedUrl) && (
-            <div className="flex flex-col items-center text-center mx-auto">
-              <h3 className="font-custom text-[1.5rem] sm:text-[1.8rem] md:text-[2.3rem] lg:text-[2.3rem] mt-4 mb-5 text-brand-darkMode tracking-widest">
-                View My Work
-              </h3>
+            <div className="flex flex-col items-center text-center">
               <div className="flex flex-row mb-9">
                 {githubUrl && (
                   <a
-                    className="border-double border-4 border-brand-darkMode hover:border-brand-darkMode px-10 mx-4 font-custom tracking-wider rounded-sm hover:text-brand-darkMode bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+                    className="border-double border-4 border-brand-lightMode dark:border-brand-darkMode hover:border-brand-darkest px-10 mx-4 font-custom tracking-wider rounded-sm hover:text-brand-lightish dark:hover:text-brand-cardbg bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
                     href={githubUrl}
                     target="_blank"
                     rel="noopener noreferrer"
@@ -150,7 +161,7 @@ const Experience: React.FC<ProjectCardProps> = () => {
                 )}
                 {deployedUrl && (
                   <a
-                    className="border-double border-4 border-brand-darkMode hover:border-brand-darkMode px-10 mx-4 font-custom tracking-wider rounded-sm hover:text-brand-darkMode bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+                    className="border-double border-4 border-brand-lightMode dark:border-brand-darkMode hover:border-brand-darkest px-10 mx-4 font-custom tracking-wider rounded-sm hover:text-brand-lightish dark:hover:text-brand-cardbg bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
                     href={deployedUrl}
                     target="_blank"
                     rel="noopener noreferrer"
@@ -161,8 +172,33 @@ const Experience: React.FC<ProjectCardProps> = () => {
               </div>
             </div>
           )}
+          <div className="bg-brand-base dark:bg-brand-darkMode w-full mx-auto pt-7 pb-7 px-9">
+            <p className="mt-4 max-w-[900px] text-4 sm:text-[1.5rem] md:text-[2rem] lg:text-[1.3rem] text-brand-darkMode dark:text-brand-lightMode tracking-widest mx-auto text-center font-custom pb-4">
+              {longDescription}
+            </p>
+          </div>
+
+          {technology && Array.isArray(technology) && (
+            <div className="bg-brand-base dark:bg-brand-darkMode flex flex-col items-center text-center mx-auto">
+              <h3 className="font-custom text-[1.5rem] sm:text-[1.8rem] md:text-[2.3rem] lg:text-[2.3rem] mb-5 text-brand-darkMode dark:text-brand-base tracking-widest">
+                Technology used
+              </h3>
+              <div className="flex flex-row">
+                {technology.map((iconUrl: string, index: number) => (
+                  <Image
+                    key={index}
+                    src={iconUrl}
+                    alt={`tech-icon-${index}`}
+                    height={100}
+                    width={100}
+                    className="w-7 h-7 mx-5 flex-row mb-10"
+                  />
+                ))}
+              </div>
+            </div>
+          )}
         </div>
-      </div>
+      )}
     </div>
   );
 };
