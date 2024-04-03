9f2658360930b4b19d9c3c89fd19b4571786b72a
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-02 20:36:37 -0400

    categorized skills

--- data/skills-data.ts
+++ data/skills-data.ts
@@ -1,89 +1,248 @@
-export const SkillsData: { image: string; description: string }[] = [
+export const SkillsData: {
+  category: string;
+  technologies: { image: string; description: string }[];
+}[] = [
   {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-    description: "React",
+    category: "Frontend Frameworks/Libraries",
+    technologies: [
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        description: "React",
+      },
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
+        description: "Next.js",
+      },
+    ],
   },
   {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
-    description: "JavaScript",
-  },
-
-  {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-    description: "TypeScript",
-  },
-
-  {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-    description: "HTML5",
-  },
-
-  {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-    description: "CSS3",
-  },
-
-  {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
-    description: "SASS",
+    category: "Styling",
+    technologies: [
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        description: "CSS3",
+      },
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+        description: "SASS",
+      },
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+        description: "Tailwind CSS",
+      },
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+        description: "Material UI",
+      },
+    ],
   },
   {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
-    description: "Tailwindcss",
+    category: "Backend Frameworks/Libraries",
+    technologies: [
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        description: "Node.js",
+      },
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+        description: "Express",
+      },
+    ],
   },
   {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
-    description: "MaterialUI",
+    category: "Database",
+    technologies: [
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+        description: "MySQL",
+      },
+    ],
   },
   {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
-    description: "Next.js",
+    category: "API/Backend Tools",
+    technologies: [
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        description: "TypeScript",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1712101206/rest-api-icon_euhh23.png",
+        description: "RESTful API's",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1712101145/icons8-json-web-token-48_v5f1xh.png",
+        description: "JWT",
+      },
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+        description: "Firebase",
+      },
+    ],
   },
   {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-    description: "Node.js",
+    category: "Version Control",
+    technologies: [
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        description: "GitHub",
+      },
+    ],
   },
   {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
-    description: "MySQL",
+    category: "CMS (Content Management System)",
+    technologies: [
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg",
+        description: "WordPress",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698363684/squarespace_logo_icon_181252_o3zs5v.png",
+        description: "SquareSpace",
+      },
+    ],
   },
   {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
-    description: "Express",
-  },
-  {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
-    description: "Firebase",
-  },
-
-  {
-    image:
-      "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1712101206/rest-api-icon_euhh23.png",
-    description: "RESTful API's",
-  },
-
-  {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
-    description: "GitHub",
-  },
-
-  {
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg",
-    description: "Wordpress",
+    category: "Other Language Tools",
+    technologies: [
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+        description: "JavaScript",
+      },
+      {
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        description: "HTML5",
+      },
+    ],
   },
 ];
+
+// export const SkillsData: {
+//   category: string;
+//   skills: { image: string; description: string }[];
+// }[] = [
+//   {
+//     category: "Front-End",
+//     skills: [
+//       {
+//         image:
+//           "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+//         description: "React",
+//       },
+//     ],
+//   },
+//   {
+//     category: "Front-End",
+//     skills: [{
+//       image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
+//     description: "Next.js",
+//     }]
+//   },
+//   {
+//     category: "Front-End",
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+//     description: "JavaScript",
+//   },
+
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+//     description: "TypeScript",
+//   },
+
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+//     description: "HTML5",
+//   },
+
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+//     description: "CSS3",
+//   },
+
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+//     description: "SASS",
+//   },
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+//     description: "Tailwindcss",
+//   },
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+//     description: "MaterialUI",
+//   },
+
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+//     description: "Node.js",
+//   },
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+//     description: "MySQL",
+//   },
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+//     description: "Express",
+//   },
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+//     description: "Firebase",
+//   },
+
+//   {
+//     image:
+//       "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1712101206/rest-api-icon_euhh23.png",
+//     description: "RESTful API's",
+//   },
+
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+//     description: "GitHub",
+//   },
+
+//   {
+//     image:
+//       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg",
+//     description: "Wordpress",
+//   },
+//   {
+//     image:
+//       "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1698363684/squarespace_logo_icon_181252_o3zs5v.png",
+//     description: "SquareSpace",
+//   },
+//   {
+//     image:
+//       "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1712101145/icons8-json-web-token-48_v5f1xh.png",
+//     description: "JWT",
+//   },
+// ];
