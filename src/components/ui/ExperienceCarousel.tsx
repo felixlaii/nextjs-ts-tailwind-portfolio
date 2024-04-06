a0addac67fa9f87a013a7548f4d5440f11be7ca5
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-06 17:55:41 -0400

    created useState

--- src/components/ui/ExperienceCarousel.tsx
+++ src/components/ui/ExperienceCarousel.tsx
@@ -1,4 +1,8 @@
+import { createRef, useState } from "react";
+
 const ExperienceCarousel: React.FC = () => {
+  const [currentImage, setCurrentImage] = useState<number>(0);
+
   return <div></div>;
 };
 
