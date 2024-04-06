2244f9dabecda9f1be78d90907dafae7fcd9977c
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-06 17:49:58 -0400

    Update ImageGrid.tsx

--- src/components/ui/ImageGrid.tsx
+++ src/components/ui/ImageGrid.tsx
@@ -2,7 +2,19 @@ import { ProjectCardProps } from "@/types/component-types";
 import { motion } from "framer-motion";
 
 const ImageGrid: React.FC<ProjectCardProps> = ({ image }) => {
-  return <div></div>;
+  return (
+    <div className="flex flex-wrap justify-center gap-4 w-full mx-auto">
+      {image.map((img, i) => (
+        <div>
+          <motion.div>
+            <a>
+              <Image width={100} height={100} />
+            </a>
+          </motion.div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ImageGrid;
