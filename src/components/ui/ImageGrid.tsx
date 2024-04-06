b7795d66f49620d54e376f68cf685c9b99293282
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-06 17:48:02 -0400

    passed in project card props

--- src/components/ui/ImageGrid.tsx
+++ src/components/ui/ImageGrid.tsx
@@ -1,6 +1,7 @@
+import { ProjectCardProps } from "@/types/component-types";
 import { motion } from "framer-motion";
 
-const ImageGrid: React.FC = () => {
+const ImageGrid: React.FC<ProjectCardProps> = ({ image }) => {
   return <div></div>;
 };
 
