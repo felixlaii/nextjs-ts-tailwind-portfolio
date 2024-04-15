aabc450e598333c291cbc82f0f98a6e00d09ed00
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-04-15 18:27:03 -0400

    fixed local storage for dark and light mode

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -4,24 +4,32 @@ import { BsSunFill } from "react-icons/bs";
 
 const ThemeToggle = () => {
   const [darkMode, setDarkMode] = useState(() => {
-    const savedMode = localStorage.getItem("theme");
-    return savedMode === "dark";
+    if (typeof window !== "undefined") {
+      const savedMode = localStorage.getItem("theme");
+      return savedMode === "dark";
+    } else {
+      return true;
+    }
   });
 
   useEffect(() => {
-    const savedMode = localStorage.getItem("theme");
-    if (savedMode === "dark") {
-      setDarkMode(true);
+    if (typeof window !== "undefined") {
+      const savedMode = localStorage.getItem("theme");
+      if (savedMode === "dark") {
+        setDarkMode(true);
+      }
     }
   }, []);
 
   useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
+    if (typeof window !== "undefined") {
+      if (darkMode) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
     }
   }, [darkMode]);
 
