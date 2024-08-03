9e63bc91f2953482d56e1addcd6512ffb39d3758
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-08-03 19:59:16 -0400

    Update portfolio-info.ts

--- data/portfolio-info.ts
+++ data/portfolio-info.ts
@@ -1,7 +1,6 @@
 export const FELIX_DETAILS = {
-    name: "Felix Lai",
-    location: "Toronto, Ontario",
-    email: "felix.lai@hotmail.com",
-    phone: "647-547-6716"
-  };
-  
+  name: "Felix Lai",
+  location: "Toronto, Ontario",
+  email: "felix@felixlaii.com",
+  phone: "647-547-6716",
+};
